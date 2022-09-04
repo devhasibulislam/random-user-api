6e416e4629785343f54db92974c3b66d44d8e923
hasibulislam999 <hasib143sl@gmail.com>
2022-09-04 18:54:34 +0600

    fetching a random user data done

---
+++ controllers/user.controller.js
@@ -0,0 +1,18 @@
+// external imports
+const fs = require("fs");
+const randomID = require("../utils/randomID");
+
+// internal import
+const users = require(__dirname + "/../public/user.json");
+
+// Get a random user data
+module.exports.getARandomUser = (req, res) => {
+    const rand = randomID(users.length);
+
+    res.status(200).json({
+        success: true,
+        message: "successfully fetching a random user data",
+        data: users.find(user => user.id === rand)
+    });
+}
+
